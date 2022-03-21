c5532193f0ffd996e3dccc97c281921b8e534946
goldWing <just.goldwing@gmail.com>
2022-03-21 00:01:31 -0300

    added services segment

--- js/main.js
+++ js/main.js
@@ -36,3 +36,53 @@ function linkAction (){
 
 navLink.forEach (n => n.addEventListener('click', linkAction))
 
+
+
+// =================== ACCORDION SKILLS ==================
+
+const skillsContent = document.getElementsByClassName('skillsContent')
+const skillsHeader =document.querySelectorAll('.skillsHeader')
+
+function toggleSkills(){
+ let itemClass = this.parentNode.className
+
+for (i = 0; i < skillsContent.length; i++){
+ skillsContent[i].className = 'skillsContent skillsClose'
+}
+if (itemClass === 'skillsContent skillsClose'){
+ this.parentNode.className = 'skillsContent skillsOpen'
+}
+
+}
+
+skillsHeader.forEach((el) => {
+ el.addEventListener('click', toggleSkills)
+})
+
+
+
+
+
+const modalViews = document.querySelectorAll('.servicesModal')
+const modalButtons = document.querySelectorAll('.servicesButton')
+const modalCloses = document.querySelectorAll('.services-modal-close')
+
+let modal = function (modalClick){
+ modalViews[modalClick].classList.add('activeModal')
+}
+
+
+modalButtons.forEach((modalButton, i) => {
+ modalButton.addEventListener('click', () => {
+  modal(i)
+ } )
+
+})
+
+modalCloses.forEach((modalClose) => {
+ modalClose.addEventListener('click', () => {
+  modalViews.forEach((modalView) => {
+   modalView.classList.remove('activeModal')
+  })
+ })
+})
