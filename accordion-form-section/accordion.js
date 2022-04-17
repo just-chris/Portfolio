8ed7c769bfc9549f8c8165d2cf1d8e2572337eaa
goldWing <just.goldwing@gmail.com>
2022-04-16 22:31:50 -0300

    implemented darkMode

--- accordion-form-section/accordion.js
+++ accordion-form-section/accordion.js
@@ -20,3 +20,31 @@ for (i = 0; i < accordion.length; i++) {
 // style.setProperty("--inputBack", "#FFF");
 // style.setProperty("--borders", "rgba(147, 47, 109, 0.33)");
 // style.setProperty("--textInput", "#420039");
+let darkMode = localStorage.getItem("darkMode");
+
+const darkModeToggle = document.querySelector("#switchInput");
+
+const enableDarkMode = () => {
+	document.body.classList.add("darkmode");
+	localStorage.setItem("darkMode", "enabled");
+};
+
+const disableDarkMode = () => {
+	document.body.classList.remove("darkmode");
+	localStorage.setItem("darkMode", null);
+};
+
+if (darkMode === "enabled") {
+	enableDarkMode();
+}
+
+darkModeToggle.addEventListener("click", () => {
+	darkMode = localStorage.getItem("darkMode");
+	if (darkMode !== "enabled") {
+		enableDarkMode();
+		console.log(darkMode);
+	} else {
+		disableDarkMode();
+		console.log(darkMode);
+	}
+});
