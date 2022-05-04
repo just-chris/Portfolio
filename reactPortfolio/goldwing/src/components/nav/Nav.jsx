2ca936cc569cf5f87a5b522ac38b854b1c32ec9f
goldWing <just.goldwing@gmail.com>
2022-05-04 00:29:34 -0300

    started reactJS portfolio

---
+++ reactPortfolio/goldwing/src/components/nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './nav.css'
+
+export const Nav = () => {
+  return (
+    <div>nav</div>
+  )
+}
+
+export default Nav;
---
+++ reactPortfolio/goldwing/src/components/xp/Exp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './exp.css'
+
+export const Exp = () => {
+  return (
+    <div>exp</div>
+  )
+}
+
+export default Exp;
