043bd4126606f7c7e09208a57469d1a8e9db76df
just-chris <just.goldwing@gmail.com>
2022-10-30 20:36:59 -0300

    finished dashboard

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -1,15 +1,76 @@
-const bg = document.querySelector('body');
-const imgLogo = document.querySelector('#imageLogo');
+const body = document.getElementById('myBody')
+const imgLogo = document.querySelector('#imageLogo')
+const sideMenu = document.querySelector('aside')
+const menu = document.getElementById('menu-btn')
+const close = document.getElementById('close-btn')
+const themeToggle = document.querySelector('.theme-toggle')
+const lightToggle = document.getElementById('light')
+const darkToggle = document.getElementById('dark')
 
 window.addEventListener('load', (e) => {
- bg.style.background = 'var(--bg-color)'
- if (bg.style.background != 'var(--bg-color)' ) {
-  imgLogo.src = "./assets/logo-light.png" 
+ 
+ // localStorage.setItem('theme', null );
+ let theme = sessionStorage.getItem('theme');
+ console.log(theme == "light")
+ if ( theme == null || theme == "light") {
+  imgLogo.src = "./assets/logo-dark.png"
+  lightToggle.classList.add('active')
+  darkToggle.classList.remove('active')
  } else {
-  imgLogo.src = "./assets/logo-dark.png";
-
-  console.log(bg.style.background)
+  imgLogo.src = "./assets/logo-light.png"
+  body.classList.toggle('dark-theme')
+  darkToggle.classList.add('active')
+  lightToggle.classList.remove('active')
  }
+} )
+
+// window.addEventListener('load', (e) => {
+//  body. = 'var(--bg-color)'
+//  if (bg.style.background != 'var(--bg-color)' ) {
+//   imgLogo.src = "./assets/logo-light.png" 
+//  } else {
+//   imgLogo.src = "./assets/logo-dark.png";
+
+//   console.log(bg.style.background)
+//  }
+// } )
+
+menu.addEventListener('click', () => {
+
+ sideMenu.style.left = "0"
+ sideMenu.style.opacity = "1"
+ sideMenu.style.visibility = "visible"
+} )
+
+
+close.addEventListener('click', () => {
+ sideMenu.style.left = "-100%"
+ sideMenu.style.opacity = "0"
+ sideMenu.style.visibility = "hidden"
+} )
+
+themeToggle.addEventListener('click', () => {
+let currentMode = sessionStorage.getItem('theme')
+body.classList.toggle('dark-theme')
+themeToggle.querySelector('span').classList.toggle('active')
+
+
+if ( currentMode == null ||currentMode == "light") {
+ darkToggle.classList.add('active')
+ lightToggle.classList.remove('active')
+ sessionStorage.setItem('theme', 'dark')  
+ imgLogo.src = "./assets/logo-light.png"
+
+ 
+} else {
+ lightToggle.classList.add('active')
+ darkToggle.classList.remove('active')
+ sessionStorage.setItem('theme', 'light')
+ imgLogo.src = "./assets/logo-dark.png"
+
+}
+
+
 } )
 
 
